4dcfcd130047022d6736906062f53c623546fec9
Rodrigo Gomes <rodrigogr1602@gmail.com>
2022-10-28 21:49:36 -0300

    config inicial

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+const app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(8000)
