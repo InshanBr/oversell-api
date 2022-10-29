7d4fe4ff4ecbeeae85cf67e59eb7d9f573fe0a59
Rodrigo Gomes <rodrigogr1602@gmail.com>
2022-10-28 23:02:45 -0300

    cria registro de usuario

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
 import express from 'express'
+import { PrismaClient } from '@prisma/client'
+import bcrypt from 'bcryptjs'
+
 const app = express()
+const prisma = new PrismaClient()
+app.use(express.json())
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.post('/login', async (req, res) => {
+  try {
+    const checkemail = await prisma.user.findFirst({
+      where: {
+        email: req.body.email
+      }
+    })
+    if (checkemail === null) {
+      const passwordHash = await bcrypt.hash(req.body.password, 12)
+      const user = await prisma.user.create({
+        data: {
+          email: req.body.email,
+          password: passwordHash,
+          username: req.body.username
+        }
+      })
+      res.json(user)
+    } else {
+      res.json({ err: 'User already exists!' })
+    }
+  } catch (err) {
+    res.json({ err: 'Error at create user!' })
+  }
 })
 
 app.listen(8000)
