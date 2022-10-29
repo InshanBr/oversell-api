3b56e218e93e6a938e12d7658451431f7a5a6348
Rodrigo Gomes <rodrigogr1602@gmail.com>
2022-10-29 11:46:03 -0300

    Update controllers.ts

--- src/controllers.ts
+++ src/controllers.ts
@@ -42,12 +42,12 @@ const login = async (req: Request, res: Response): Promise<any> => {
     } else {
       bcrypt.compare(req.body.password, checkemail.password, (err, compareRes) => {
         if (err) { // error while comparing
-          res.status(502).json({ message: 'error while checking user password' })
+          res.status(502).json({ err: 'error while checking user password' })
         } else if (compareRes) { // password match
           const token = jwt.sign({ email: req.body.email }, 'secret', { expiresIn: '1h' })
           res.status(200).json({ message: 'user logged in', token })
         } else { // password doesnt match
-          res.status(401).json({ message: 'invalid credentials' })
+          res.status(401).json({ err: 'invalid credentials' })
         };
       })
     }
@@ -59,17 +59,17 @@ const login = async (req: Request, res: Response): Promise<any> => {
 const isAuth = async (req: Request, res: Response): Promise<any> => {
   const authHeader = req.get('Authorization')
   if (!authHeader) {
-    return res.status(401).json({ message: 'not authenticated' })
+    return res.status(401).json({ err: 'not authenticated' })
   };
   const token = authHeader.split(' ')[1]
   let decodedToken
   try {
     decodedToken = jwt.verify(token, 'secret')
   } catch (err: any) {
-    return res.status(500).json({ message: err.message || 'could not decode the token' })
+    return res.status(500).json({ err: err.message || 'could not decode the token' })
   };
   if (!decodedToken) {
-    res.status(401).json({ message: 'unauthorized' })
+    res.status(401).json({ err: 'unauthorized' })
   } else {
     res.status(200).json({ message: 'here is your resource' })
   }
