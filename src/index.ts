2e7f09a26ec96b3dd3c9d9b4ba532cd24dfce6ad
preduh <pedro.lucas5860@gmail.com>
2022-10-31 20:53:24 -0300

    chore: adiciona e configura o CORS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express'
 import { signup, login, isAuth } from './controllers'
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+  origin: '*'
+}))
 
 app.post('/signup', signup)
 app.post('/login', login)
