4ff5fca4737ebed5741ba28c3e799c367535ab93
Rodrigo Gomes <rodrigogr1602@gmail.com>
2023-03-01 01:05:02 -0300

    feat: adiciona rota para alteração de senha

--- src/controllers/auth/change.controller.ts
+++ src/controllers/auth/change.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import { prismaClient } from '../../database/client'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcryptjs'
 
 interface DecodedToken {
   id: string
@@ -52,5 +53,53 @@ export const Change = async (
   } catch (err: any) {
     console.log(err)
     return res.status(400).json({ error: 'Failed to change info of the user' })
+  }
+}
+
+export const Password = async (
+  req: Request,
+  res: Response
+): Promise<Response<any, Record<string, any>> | undefined> => {
+  try {
+    const { password, newPassword } = req.body
+    const { authorization } = req.headers
+
+    if (!authorization) return res.status(400).json({ error: 'Missing token' })
+    if (!password) return res.status(400).json({ error: 'Missing Password' })
+    if (!newPassword) return res.status(400).json({ error: 'Missing New Password' })
+
+    const token = authorization.split(' ')[1]
+
+    const decodedToken = jwt.verify(token, 'secret') as DecodedToken
+
+    if (!decodedToken) { return res.status(400).json({ error: 'Failed to decode token' }) }
+
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: decodedToken.id
+      }
+    })
+
+    if (!user) return res.status(400).json({ error: 'Email/Password is invalid' })
+
+    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+
+    if (!passwordIsCorrect) return res.status(400).json({ error: 'Password is invalid' })
+
+    const passwordHash = await bcrypt.hash(newPassword, 12)
+
+    const update = await prismaClient.user.update({
+      where: { id: decodedToken.id },
+      data: {
+        password: passwordHash
+      }
+    })
+
+    if (!update) return res.status(400).json({ error: 'Password change failed' })
+
+    return res.status(200).json({ message: 'Password changed' })
+  } catch (err: any) {
+    console.log(err)
+    return res.status(400).json({ error: 'Failed to change password' })
   }
 }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,13 +2,14 @@ import { Router } from 'express'
 import { isAuth } from '../controllers/auth/isAuth.controller'
 import { SignIn } from '../controllers/auth/signIn.controller'
 import { SignUp } from '../controllers/auth/signUp.controller'
-import { Change } from '../controllers/auth/change.controller'
+import { Change, Password } from '../controllers/auth/change.controller'
 
 const authRouter = Router()
 
 authRouter.post('/signup', SignUp)
 authRouter.post('/login', SignIn)
 authRouter.put('/change', Change)
+authRouter.put('/password', Password)
 authRouter.get('/isAuth', isAuth)
 
 export { authRouter }
